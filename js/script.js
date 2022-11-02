b8cc0b3d41efdeea29da1e16165286d306a09539
Lucas Augusto <lucasaugustoleon@gmail>
2022-11-02 17:18:46 -0300

    fix2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 //Elements selectors 
 
 const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const cancelEdit = document.querySelector('#cancel-edit-btn');
+const searchInput = document.querySelector('#search-input');
+const eraseButton = document.querySelector('#erase-button');
+const filterSelect = document.querySelector('#filter-select')
+
+
+// Functions
+
+
+
+// Events
